22acaaf00ce5576690f897097b5fc395db307732
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-03-28 03:16:57 +0700

    migration, database seeder, factory

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
@@ -13,7 +14,7 @@ class Todo extends Model
 
         'user_id',
 
-        is_complete,
+        'is_complete',
     ];
 
     public function user(){
