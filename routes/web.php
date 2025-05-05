ef468ebe01011f0c3efe33f7dae2a4428cab9e69
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-05-05 21:21:12 +0700

    middleware, gate admin

--- routes/web.php
+++ routes/web.php
@@ -39,9 +39,13 @@ Route::middleware('auth')->group(function () {
     
 
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
+});
+
+Route::middleware(['auth', 'admin']) ->group(function(){
+    
+    Route::resource('user', UserController::class)->except(['show']);
     Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
     Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
-
 });
 
 require __DIR__.'/auth.php';
---
+++ tugas7.md
@@ -0,0 +1,6 @@
+# Tugas 7
+
+## 1. Middleware & Gate Admin Not Active
+![Alt text](screenshot/tugas7/notActive_mdlwr.png)
+## 2. Middleware & Gate Admin Active
+![Alt text](screenshot/tugas7/active_mdlwr.png)
